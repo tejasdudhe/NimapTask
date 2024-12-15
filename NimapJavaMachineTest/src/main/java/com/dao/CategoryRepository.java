19eae88ff57e4cd1ff68070b5068e5a03f7eb609
“tejasdudhe” <tejasdudhe12@gmail.com>
2024-12-16 01:52:57 +0530

    NimapJavaMachineTest Initial commit

---
+++ NimapJavaMachineTest/src/main/java/com/dao/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.model.Category;
+
+@Repository
+public interface CategoryRepository  extends JpaRepository<Category, Integer>
+{
+
+}
---
+++ NimapJavaMachineTest/src/main/java/com/services/CategoryService.java
@@ -0,0 +1,43 @@
+package com.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.dao.CategoryRepository;
+import com.model.Category;
+
+@Service
+public class CategoryService {
+	
+	 @Autowired
+	 private CategoryRepository categoryRepo;
+
+	 public Page<Category> getAllCategories(int page, int size) {
+	     return categoryRepo.findAll(PageRequest.of(page,size));
+	 }
+	 
+	
+	 public Category createCategory(Category category) {
+	     return categoryRepo.save(category);
+	 }
+
+	 public Optional<Category> getCategoryById(int categID) {
+	     return categoryRepo.findById(categID);
+	 }
+	 
+	 
+	 public Category updateCategory(int categID, Category categoryDetails) {
+	     Category category = categoryRepo.findById(categID).orElseThrow(() -> new RuntimeException("Category not found"));
+	     category.setCategName(categoryDetails.getCategName());
+	     return categoryRepo.save(category);
+	 }
+
+	 public void deleteCategory(int categID) {
+		 categoryRepo.deleteById(categID);
+	 }
+
+}
